3da191a1ad618e5a1d6ddf1143726e62e39521e1
박정현 <bagjeonghyeon@bagjeonghyeons-MacBook-Pro-2.local>
2025-02-06 21:27:48 +0900

    15989.cpp

--- workbook_growth/15989.cpp
+++ workbook_growth/15989.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 
 using namespace std;
-int t,n;
-int dp[10005];
+
+int dp[10005][4];
+int t;
 
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    cin>>t;
-
-    dp[1] = 1;
-    dp[2] = 1;
-    dp[3] = 2;
-    for(int i=4;i<=10000;i++) dp[i] = dp[i-1] + 1;
-
-    for(int i=0;i<n;i++){
-        cin>>n;
-
+    dp[0][1] = 1;
+    for(int i = 1; i <= 10000; i++){
+        dp[i][1] = dp[i-1][1];
+        if(i>=2) dp[i][2] = dp[i-2][1] + dp[i-2][2];
+        if(i>=3) dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
     }
     
+    cin>>t;
+    int n;
+    for(int i = 0; i < t; i++){
+        cin >> n;
+        cout << dp[n][1] + dp[n][2] + dp[n][3] <<'\n';
+    }
 }
